56fc079779097b0296cf2680ce373cf8611d71c5
italo macedo <macedoitalo21@outlook.com>
2025-05-17 20:42:58 -0300

    Adicao de vercel.json e url base render

--- InvictusAPP/vite.config.ts
+++ InvictusAPP/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: process.env.NODE_ENV === 'development' ? {
       '/api': {
-        target: process.env.VITE_BASE_URL || 'http://localhost:5000', // URL do backend
+        target: process.env.VITE_BASE_URL_PROD || 'http://localhost:5000', // URL do backend
         changeOrigin: true,
         secure: false,
       },
