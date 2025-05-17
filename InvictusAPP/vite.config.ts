cd407d1632f5e7f8c324d44b51204c58fbddfc51
italo macedo <macedoitalo21@outlook.com>
2025-05-17 20:23:00 -0300

    front-end-invictus

--- InvictusAPP/vite.config.ts
+++ InvictusAPP/vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv'
+
+// Carrega as variáveis de ambiente do arquivo .env
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-        '/api': {
-            target: 'http://localhost:5000', // URL do backend
-            changeOrigin: true,
-            secure: false,
-        },
+      '/api': {
+        target: process.env.VITE_BASE_URL || 'http://localhost:5000', // URL do backend
+        changeOrigin: true,
+        secure: false,
+      },
     },
-},
+  },
 })
